2c9450b543c0f83dc6fd225d8e3bc9b9bd2cd373
Harsh Agrawal <harshagrawal523@gmail.com>
2023-07-05 09:47:50 +0900

    sb better

--- Agent.py
+++ Agent.py
@@ -525,55 +525,55 @@ class Agent():
   #         current_y += text_surface.get_height()
   
   def speech_bubble(self):
-    text = self.msg
-    x = self.x
-    y = self.y
-    bubble_image = pygame.image.load(Path+"speechbubble.png")  # Replace "bubble.png" with the path to your predetermined image
-    bubble_rect = bubble_image.get_rect()
-    # bubble_rect.center = (x, y)
+      text = self.msg
+      x = self.x
+      y = self.y
+      bubble_image = pygame.image.load(Path+"speechbubble.png")  # Replace "bubble.png" with the path to your predetermined image
 
-    # Render the text
-    font_size = 22  # Desired font size
-    font = pygame.font.Font(None, font_size)
+      # Render the text
+      font_size = 22  # Desired font size
+      font = pygame.font.Font(None, font_size)
 
-    # Split text into words
-    words = text.split()
+      # Split text into words
+      words = text.split()
 
-    # Create lines of text with a maximum of 6 words per line
-    text_lines = []
-    line = ""
-    for word in words:
-        if len(line.split()) < 6:
-            line += " " + word
-        else:
-            text_lines.append(line.strip())
-            line = word
-    text_lines.append(line.strip())
+      # Create lines of text with a maximum of 6 words per line
+      text_lines = []
+      line = ""
+      for word in words:
+          if len(line.split()) < 6:
+              line += " " + word
+          else:
+              text_lines.append(line.strip())
+              line = word
+      text_lines.append(line.strip())
 
       # Calculate the maximum width and height for all lines
-    max_width = 0
-    total_height = 0
-    for line in text_lines:
-        text_surface = font.render(line, True, (0, 0, 0))
-        max_width = max(max_width, text_surface.get_width())
-        total_height += text_surface.get_height()
+      max_width = 0
+      total_height = 0
+      for line in text_lines:
+          text_surface = font.render(line, True, (0, 0, 0))
+          max_width = max(max_width, text_surface.get_width())
+          total_height += text_surface.get_height()
 
-    # Create the bubble rectangle around the text
-    bubble_padding = 10
-    bubble_width = max_width + bubble_padding * 2
-    bubble_height = total_height + bubble_padding * 2
-    bubble_rect = pygame.Rect(x - bubble_width // 2 - 50, y - bubble_height // 2 -50, bubble_width, bubble_height)
+      # Create the bubble rectangle around the text
+      bubble_padding = 10
+      bubble_width = max_width + bubble_padding * 2
+      bubble_height = total_height + bubble_padding * 2
+      bubble_rect = pygame.Rect(x - bubble_width // 2, y - bubble_height // 2, bubble_width, bubble_height)
 
-    # Blit the bubble image onto the surface
-    self.win.blit(bubble_image, bubble_rect)
+      # Blit the bubble image onto the surface
+      scaled_bubble_image = pygame.transform.scale(bubble_image, (bubble_width, bubble_height))
+      bubble_rect = scaled_bubble_image.get_rect(center=(x, y))
+      self.win.blit(scaled_bubble_image, bubble_rect)
 
-    # Blit the text onto the bubble
-    current_y = bubble_rect.top + 10  # Adjust the vertical position of the text
-    for line in text_lines:
-        text_surface = font.render(line, True, (0, 0, 0))
-        text_rect = text_surface.get_rect(centerx=bubble_rect.centerx, top=current_y)
-        self.win.blit(text_surface, text_rect)
-        current_y += text_surface.get_height()
+      # Blit the text onto the bubble
+      current_y = bubble_rect.top + bubble_padding
+      for line in text_lines:
+          text_surface = font.render(line, True, (0, 0, 0))
+          text_rect = text_surface.get_rect(centerx=bubble_rect.centerx, top=current_y)
+          self.win.blit(text_surface, text_rect)
+          current_y += text_surface.get_height()
 
 
   def emoji_bubble(self, emoji):
