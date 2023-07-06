e12300bde1b6167bb9c07dfea0dfbddcc65c3870
chakshudhannawat <chakshu.dhannawat1@gmail.com>
2023-07-06 10:48:52 +0900

    Shape_chnage Mark02

--- shape_change.py
+++ shape_change.py
@@ -1,4 +1,5 @@
 import math
+from Params import *
 
 def update_coordinate(coord, original_size, new_size):
     original_x, original_y = original_size
@@ -49,6 +50,37 @@ new_window_size = (2000, 1500)
 updated_radius = update_radius(original_radius, original_window_size, new_window_size)
 print(updated_radius)  # Output: 63
 
+
+
+##################################################################################
+
+# LOCATION_MAP change
+
+def update_location_map(location_map, original_size, new_size):
+    updated_location_map = {}
+    original_x, original_y = original_size
+    new_x, new_y = new_size
+
+    for location, coordinate in location_map.items():
+        # Calculate the updated coordinates
+        updated_x = int(coordinate[0] * new_x / original_x)
+        updated_y = int(coordinate[1] * new_y / original_y)
+
+        # Add the updated coordinates to the updated location map
+        updated_location_map[location] = (updated_x, updated_y)
+
+    # Convert the updated location map to a string representation
+    map_string = str(updated_location_map)
+
+    return map_string
+
+
+
+original_window_size = (800, 600)
+new_window_size = (1024, 768)
+
+updated_map_string = update_location_map(LOCATION_MAP, original_window_size, new_window_size)
+print(updated_map_string)
 
 
 
