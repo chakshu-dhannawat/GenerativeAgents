f6bd31535cb7feb8c5ea9d989678762500d7ce6a
Aishwarya Chodavarapu <aishwaryachodavarapu@gmail.com>
2023-07-19 10:03:06 +0900

    town final image

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
 import os
 N_Farewell_T = sum([len(files) for _, _, files in os.walk('Assets/Farewell/Townfolk')])
 print(N_Farewell_T)
 
 for _, _, files in os.walk('Assets/Farewell/Townfolk'):
     print(files)
+=======
+>>>>>>> Stashed changes
 =======
 >>>>>>> Stashed changes
